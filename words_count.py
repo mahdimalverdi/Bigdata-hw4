9ae43c8f1eabd6cb344fce455e5cf86edb0ffd34
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-04-01 07:07:16 +0430

    try fix

--- words_count.py
+++ words_count.py
@@ -8,10 +8,10 @@ output = sys.argv[2]
 if __name__ == "__main__":
 	sc = SparkContext(conf=SparkConf())
 
-	f = open("log.txt", "a")
-	f.write("Hello World!")
-	f.close()
-	
+	log4jLogger = sc._jvm.org.apache.log4j 
+	log = log4jLogger.LogManager.getLogger(__name__) 
+	log.info("Hello World!")
+
 	words = sc.textFile(input).flatMap(lambda line: line.split())
 	wordCounts = words.map(lambda word: (word.lower(), 1)).reduceByKey(lambda a,b:a +b)
 	wordCounts.saveAsTextFile(output)

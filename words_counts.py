77fe62af513b0dddc2e29a69b57348dc7989acea
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-04-01 05:03:39 +0430

    add smaple

--- words_counts.py
+++ words_counts.py
@@ -1,11 +1,15 @@
 import sys
 from pyspark import SparkContext, SparkConf
  
-input = "hdfs://localhost/user/ebrahimi/file500.txt"
-output = "output500"
+#input = "hdfs://localhost/user/ebrahimi/file500.txt"
+input = "sample.txt"
+output = "output"
 
 if __name__ == "__main__":
 	sc = SparkContext("local","Word Count")
-	words = sc.textFile(input).flatMap(lambda line: line.split(" "))
+	print("start")
+	words = sc.textFile(input).flatMap(lambda line: line.split())
+	print("read")
 	wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
 	wordCounts.saveAsTextFile(output)
+	print("write")

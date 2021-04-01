618f51ba4d047511892b2dab42cfe982480f96c5
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-04-01 05:20:29 +0430

    try fix

--- words_count.py
+++ words_count.py
@@ -2,8 +2,8 @@ import sys
 from pyspark import SparkContext, SparkConf
  
 #input = "hdfs://localhost/user/ebrahimi/file500.txt"
-input = "/user/ebrahimi/file500.txt"
-output = "output"
+input = "hw-4/sample.txt"
+output = "hw-4/output"
 
 if __name__ == "__main__":
 	conf = SparkConf()

7f895d82a902cbaac137e2e9e7e97981547cbfa3
Mahdi Malverdi <Mahdi.Malverdi@gmail.com>
2021-04-03 19:06:16 +0430

    remove tolower

--- words_count.py
+++ words_count.py
@@ -1,14 +1,15 @@
 import sys
 import time
 from pyspark import SparkContext, SparkConf
-#input = "hdfs://localhost/user/ebrahimi/file500.txt"
+
 input = sys.argv[1]
 output = sys.argv[2]
 
 if __name__ == "__main__":
-	start_time = time.time()
-	sc = SparkContext(conf=SparkConf())
-	words = sc.textFile(input).flatMap(lambda line: line.split())
-	wordCounts = words.map(lambda word: (word.lower(), 1)).reduceByKey(lambda a,b:a +b)
-	wordCounts.saveAsTextFile(output)
-	print("--- %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
+    sc = SparkContext(conf=SparkConf())
+    words = sc.textFile(input).flatMap(lambda line: line.split())
+    wordCounts = words.map(lambda word: (word, 1)
+                           ).reduceByKey(lambda a, b: a + b)
+    wordCounts.saveAsTextFile(output)
+    print("--- %s seconds ---" % (time.time() - start_time))
